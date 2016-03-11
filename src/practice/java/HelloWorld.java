11d7e19a5c281c536d502c483b46a0b1ccf2952c
Sarangan Rajamanickam <sarajama@microsoft.com>
2016-03-10 21:10:14 -0800

    This is the first commit for this project. Created assignment for hello world.

---
+++ src/practice/java/HelloWorld.java
@@ -0,0 +1,11 @@
+package practice.java;
+
+/*
+ * Assignment 1: 
+ * 		1. Write a main function in the Hello World class.
+ * 		2. The main function should display "Hello World!!!' to the console. 
+ */
+
+public class HelloWorld {
+	
+}
