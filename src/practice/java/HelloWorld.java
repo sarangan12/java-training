45bf298072b7431233f9db81b644d6c9aeac8bef
Sarangan Rajamanickam <sarajama@microsoft.com>
2016-03-10 21:29:08 -0800

    Second commit. Added a simple ifelse assignment

--- src/practice/java/HelloWorld.java
+++ src/practice/java/HelloWorld.java
@@ -1,9 +1,8 @@
 package practice.java;
 
-/*
- * Assignment 1: 
- * 		1. Write a main function in the Hello World class.
- * 		2. The main function should display "Hello World!!!' to the console. 
+/* 
+ * 	1. Write a main function in the Hello World class.
+ * 	2. The main function should display "Hello World!!!' to the console. 
  */
 
 public class HelloWorld {
---
+++ src/practice/java/IfElse_One.java
@@ -0,0 +1,15 @@
+package practice.java;
+
+/*
+ * 	1. Create an integer variable with value 10.
+ * 	2. Now check if the variable is a multiple of 2.
+ * 	3. If so, print "Number 10 is a multiple of 2". 
+ * 	4. You don't have to print anything otherwise.
+ * 	5. Repeat the assignment for various integers 79, 91, 120. 
+ *	   (Keep the divisor 2 constant) 
+ */
+public class IfElse_One {
+	public static void main(String[] args) {
+		
+	}
+}
